d4030e62bb2af20ccd55804e68231d876fde0a1b
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-04-19 20:22:13 -0500

    Habilito CORS

--- app/index.js
+++ app/index.js
@@ -10,6 +10,17 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 // Routes
 app.use(require("./routes/index"));
 app.use("/", express.static(__dirname + "/public"));
