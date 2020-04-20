c0a9652ea13776c07e156c4ec127d6f5a6c827ab
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-04-20 16:28:08 -0500

    Frontend

--- app/index.js
+++ app/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/", express.static(__dirname + "/public"));
-app.use("/", require("./routes/index"));
+app.use("/web", require("./routes/index"));
 app.use("/api", require("./routes/api"));
 app.use("/api/iot", require("./routes/iot"));
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,10 @@ router.get("/device/:device", (req, res) => {
   res.render("index", { device: req.params.device });
 });
 
+router.get("/device/:device/:sensor", (req, res) => {
+  res.render("realtime", { device: req.params.device, sensor: req.params.sensor });
+});
+
 router.get("/test", (req, res) => {
   const data = {
     message: "Hello World",
